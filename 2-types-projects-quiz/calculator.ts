5215eb0324b554898917883f0661f05d29cfc243
tjdgus <tjdgus7562@naver.com>
2022-10-21 09:31:56 +0900

    이동 함수 만들기

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,38 +1,40 @@
-/**
- * Let's make a calculator 🧮
- */
+{
+    /**
+     * Let's make a calculator 🧮
+     */
 
-type Command = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+    type Command = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
 
-function calculate(command:Command, a:number, b:number): number{
-    let result:number;
+    function calculate(command:Command, a:number, b:number): number{
+        let result:number;
 
-    switch (command){
-        case 'add':
-            result = a + b;
-            break;
-        case 'substract':
-            result = a - b;
-            break;
-        case 'multiply':
-            result = a * b;
-            break;
-        case 'divide':
-            result = a / b;
-            break;
-        case 'remainder':
-            result = a % b;
-            break;
-        default:
-            throw Error('unknown command');
+        switch (command){
+            case 'add':
+                result = a + b;
+                break;
+            case 'substract':
+                result = a - b;
+                break;
+            case 'multiply':
+                result = a * b;
+                break;
+            case 'divide':
+                result = a / b;
+                break;
+            case 'remainder':
+                result = a % b;
+                break;
+            default:
+                throw Error('unknown command');
+        }
+
+        return result;
     }
 
-    return result;
+
+    console.log(calculate('add', 1, 3)); // 4
+    console.log(calculate('substract', 3, 1)); // 2
+    console.log(calculate('multiply', 4, 2)); // 8
+    console.log(calculate('divide', 4, 2)); // 2
+    console.log(calculate('remainder', 5, 2)); // 1
 }
-
-
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
