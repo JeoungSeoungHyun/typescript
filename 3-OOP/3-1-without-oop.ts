33fff6925b4ba5863b6b2f5423989c2b9bd3a11d
tjdgus <tjdgus7562@naver.com>
2022-10-25 09:26:49 +0900

    절차지향적 커피기계 만들기

---
+++ 3-OOP/3-1-without-oop.ts
@@ -0,0 +1,29 @@
+{
+    const BEANS_GRAMM_PER_SHOT:number = 7; // 타입추론이 되기 때무에 적지 않아도 무관하긴하다.
+    let cooffeeBeans:number = 0;
+
+    type CoffeeCup = {
+        shots: number;
+        hasMilek: boolean;
+    };
+
+    function makeCoffee(shots:number):CoffeeCup{
+
+        if(cooffeeBeans < shots * BEANS_GRAMM_PER_SHOT){
+            throw new Error('Not enough coffee beans!');
+        }
+
+        cooffeeBeans -= shots * BEANS_GRAMM_PER_SHOT;
+        return {
+            shots, // 키와 밸류의 이름이 동일하다면 키의 생략이 가능
+            hasMilek: false,
+        }
+    }
+
+    cooffeeBeans += 3 * BEANS_GRAMM_PER_SHOT;
+
+    const coffee = makeCoffee(2);
+
+    console.log(coffee);
+    
+}
