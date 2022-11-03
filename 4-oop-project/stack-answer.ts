2852204b40a6e3bd13eddca03f44fc727ab697b9
JeoungSeoungHyun <tjdgus7562@naver.com>
2022-11-04 08:52:36 +0900

    힌트를 보고 다시 구현해보기(실패)

--- 4-oop-project/stack-answer.ts
+++ 4-oop-project/stack-answer.ts
@@ -1,11 +1,55 @@
 {
     interface Stack {
-        readonly size:number; // 값의 변경이 불가능하도록
+        size:number; // 값의 변경이 불가능하도록
         push(value:string): void;
         pop():string;
     }
-
+    
     // 연결리스트를 활용할것
 
+    class LinkedList {
+        static head:Node;
+
+        push(value:string):void{
+            let node = new Node(value);
+            LinkedList.head = node;
+        }
+
+        pop():string{
+            let node = LinkedList.head;
+            return node.value;
+        }
+    }
+
+    class Node {
+        value:string;
+        readonly size:number;
+        prev_Node;
+
+        constructor(value:string){
+            value;
+
+            if(LinkedList.head == null){
+                this.prev_Node = 'null';
+            } else {
+                this.prev_Node = LinkedList.head;
+            }
+        };
+    }
+
+    class Mstack implements Stack {
+        ll: LinkedList;
+        readonly size: number;
+        constructor(){
+            let ll = new LinkedList();
+            this.ll = ll;
+        }
+        push(value: string): void {
+            this.ll.push(value);
+        }
+        pop(): string {
+            return this.ll.pop();
+        }
+    }
 
 }
