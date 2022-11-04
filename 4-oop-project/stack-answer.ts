ed2d418e011d929640ad11835afb6d8bbeeac73e
JeoungSeoungHyun <tjdgus7562@naver.com>
2022-11-04 09:19:44 +0900

    stack 자료구조 구현해보기 정답

--- 4-oop-project/stack-answer.ts
+++ 4-oop-project/stack-answer.ts
@@ -1,55 +1,56 @@
 {
     interface Stack {
-        size:number; // 값의 변경이 불가능하도록
+        readonly size:number; // 값의 변경이 불가능하도록 
         push(value:string): void;
         pop():string;
     }
+
+    type StackNode = { // 값이 변경되지 않도록 만들어주기
+        readonly value: string;
+        readonly next?: StackNode;
+    }
     
-    // 연결리스트를 활용할것
+    class StackImpl implements Stack {
+        private _size: number = 0;
+        private head?: StackNode;
 
-    class LinkedList {
-        static head:Node;
+        constructor(private capacity:number) {}
 
-        push(value:string):void{
-            let node = new Node(value);
-            LinkedList.head = node;
+        get size() {
+            return this._size;
         }
 
-        pop():string{
-            let node = LinkedList.head;
-            return node.value;
-        }
-    }
-
-    class Node {
-        value:string;
-        readonly size:number;
-        prev_Node;
-
-        constructor(value:string){
-            value;
-
-            if(LinkedList.head == null){
-                this.prev_Node = 'null';
-            } else {
-                this.prev_Node = LinkedList.head;
-            }
-        };
-    }
-
-    class Mstack implements Stack {
-        ll: LinkedList;
-        readonly size: number;
-        constructor(){
-            let ll = new LinkedList();
-            this.ll = ll;
-        }
         push(value: string): void {
-            this.ll.push(value);
+            if(this._size == this.capacity){
+                throw new Error('Stack is full!');
+            }
+
+            const node: StackNode = {value, next:this.head};
+            this.head = node;
+            this._size ++;
         }
-        pop(): string {
-            return this.ll.pop();
+
+        pop(): string { 
+            if(this.head != null){// null과 undefiend는 다른 타입이지만 null == undefined 는 동일하다고 간주된다.
+                const node = this.head;
+                this.head = node.next;
+                this._size --;
+                return node.value;
+            }
+            throw new Error('Stack is empty!');
         }
     }
+
+    const stack = new StackImpl(2);
+    stack.push('test1');
+    stack.push('test2');
+    stack.push('test3');
+
+    while(stack.size != 0){
+        console.log(stack.pop());
+    }
+
+    stack.pop();
+
 
 }
