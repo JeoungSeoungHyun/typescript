f0758635253da3d9955b1104adce420a6c7ad685
JeoungSeoungHyun <tjdgus7562@naver.com>
2022-11-18 09:31:12 +0900

    etc utility type

--- 9-ADVANCED/9-9-record.ts
+++ 9-ADVANCED/9-9-record.ts
@@ -7,6 +7,9 @@
     const nav: Record<Page, PageInfo> = {
         home: {title: 'Home'},
         about : {title: 'About'},
-        contact : {title: 'Contact'}
+        contact : {title: 'Contact'},
     }
+
+    type Product = 'cat' | 'dog';
+    type NewProduct = Capitalize<Product>; // 대문자로 만들어준다. 이와 같이 다양한 utility type이 존재 한번쯤 보는걸 추천
 }
